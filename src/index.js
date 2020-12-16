6f924cdd027d97a3c8750bcf006bc604e21f2aa1
Colton Kaiser <kaiserc2@seattleu.edu>
2020-12-16 11:59:26 -0800

    Add prettier.config.js to allow formatting.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
-import { 
-  ApolloProvider, 
-  ApolloClient, 
-  createHttpLink, 
-  InMemoryCache 
-} from '@apollo/client'
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
