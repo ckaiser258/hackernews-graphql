234de34ac2f772216fc9411cb43209fbe365f1de
Colton Kaiser <kaiserc2@seattleu.edu>
2020-12-21 12:20:48 -0800

    Add useMutation and parameters to CreateLink.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,9 +1,26 @@
+import { gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
 
 const CreateLink = () => {
   const [formState, setFormState] = useState({
     description: "",
     url: "",
+  });
+  const CREATE_LINK_MUTATION = gql`
+    mutation PostMutation($description: String!, $url: String!) {
+      post(description: $description, url: $url) {
+        id
+        createdAt
+        description
+        url
+      }
+    }
+  `;
+  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+    variables: {
+      description: formState.description,
+      url: formState.url,
+    },
   });
   return (
     <div>
