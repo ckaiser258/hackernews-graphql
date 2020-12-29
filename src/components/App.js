9e11e27ec3bfab448e6cfef2e5abd445ba7934f5
Colton Kaiser <kaiserc2@seattleu.edu>
2020-12-29 13:02:20 -0800

    Add Header and routes to LinkList and CreateLink.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import "../styles/App.css";
+import React from "react";
 import CreateLink from "./CreateLink";
+import Header from "./Header";
 import LinkList from "./LinkList";
+import { Route, Switch } from "react-router";
 
 function App() {
-  return <CreateLink />;
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
